ff0f8c8121b3c5aeca1c01524165532112aebd40
Sabyasachi <lincolnmohanty.29@gmail.com>
2019-06-22 20:26:24 +0530

    included comments to repository file

--- src/main/java/com/trail/project/mongointegration/repository/UserRepository.java
+++ src/main/java/com/trail/project/mongointegration/repository/UserRepository.java
@@ -3,8 +3,13 @@ package com.trail.project.mongointegration.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.trail.project.mongointegration.document.UserDocument;
 
-
-public interface UserRepository extends MongoRepository<UserDocument, String>{
+/**
+ * Similar to JpaRepository, Spring provides MongoRepository for MongoDB to
+ * save, update , delete and retrieve a document in MongoDB.
+ * 
+ * @author lincoln
+ *
+ */
+public interface UserRepository extends MongoRepository<UserDocument, String> {
 	UserDocument findByUserId(String userId);
 }
-
